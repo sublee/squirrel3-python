e1e0eb6f7b889603ca764df5b60c3f2dc94540cf
Heungsub Lee <sub@subl.ee>
2017-03-01 03:25:54 +0900

    Port Squirrel3 into Python

---
+++ squirrel3.py
@@ -0,0 +1,39 @@
+# ~!~ coding: utf-8 ~!~
+"""
+   squirrel3
+   ~~~~~~~~~
+
+   Provides a noise function which is fast and had good distribution.  It was
+   introduced by Squirrel Eiserloh at 'Math for Game Programmers: Noise-based
+   RNG', GDC17.
+
+   :copyright: (c) 2017 by Heungsub Lee.
+   :license: BSD, see LICENSE for more details.
+"""
+
+
+__all__ = ['squirrel3']
+
+
+# The base bit-noise constants were crafted to have distinctive and interesting
+# bits, and have so far produced excellent experimental test results.
+NOISE1 = 0xb5297a4d  # 0b0110'1000'1110'0011'0001'1101'1010'0100
+NOISE2 = 0x68e31da4  # 0b1011'0101'0010'1001'0111'1010'0100'1101
+NOISE3 = 0x1b56c4e9  # 0b0001'1011'0101'0110'1100'0100'1110'1001
+
+
+def squirrel3(n, seed=0):
+    """Returns an unsigned integer containing 32 reasonably-well-scrambled
+    bits, based on a given (signed) integer input parameter `n` and optional
+    `seed`.  Kind of like looking up a value in an infinitely large
+    non-existent table of previously generated random numbers.
+    """
+    n *= NOISE1
+    n += seed
+    n ^= n >> 8
+    n += NOISE2
+    n ^= n << 8
+    n *= NOISE3
+    n ^= n >> 8
+    # Cast into uint32 like the original `Squirrel3`.
+    return n % (1 << 32)
